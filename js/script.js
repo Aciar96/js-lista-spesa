0a4e1ae432714c3b5e9154e6ff71e9600f02e270
Aciar96 <alfredo.ciarmiello96@gmail.com>
2022-06-28 17:10:11 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// Rivediamo l’esercizio della lista della spesa visto insieme, ma questa volta con il ciclo while al posto del ciclo for. 
+// Facciamo attenzione a non dimenticare tutte le differenze implementative che il ciclo while ci impone, 
+// in particolare alla variabile di indice. 
+
+// creiamo un arrey contenetnte la lista della spesa da inserire poi
+let shopping = document.getElementById('shopping');
+const sohpItems = ['Uova', 'pane', 'Latte' , ' Biscotti' ,' Pasta', ' Affettati' , 'Formaggio' , '  Verdura',' Frutta'];
+console.log(sohpItems)
+// // ciclo for
+// for(let i = 0; i <sohpItems.lengt ; i++){
+    //         listItem += `<li>${sohpItems[i]}</li>`
+    //     }
+    
+    // shopping.innerHTML = listItem;
+    
+    
+    let i = 0 ;
+    
+    let listItem = ''
+    // ciclo while
+//    condizione : 
+while(sohpItems.lengt){
+    //monto i <li> da inserire in html dinamicamente 
+    listItem += `<li>${sohpItems[i]}</li>`
+    console.log(sohpItems[i])
+    i++
+  console.log(sohpItems[i])
+}
+shopping.innerHTML = listItem;
